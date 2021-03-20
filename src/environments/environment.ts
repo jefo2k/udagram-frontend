08741752a6230c6fe3f98b937f48dd3d52e17f0b
Jeferson Santos <jefo2k@gmail.com>
2021-03-20 19:02:17 -0300

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  appName: 'Udagram',
+  apiHost: 'http://a42e453b6ae214c3d94e6f6ea973076f-1721020294.us-east-2.elb.amazonaws.com:8080/api/v0'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
